374ab14be275bfc0b73236e1eb1ab6a282189a31
beatrizsilvaqueiroz <beatriz.squeiroz7@senacsp.edu.br>
2025-02-26 14:48:59 -0300

    upload dos codigos da primeira aula

---
+++ aula01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int idade = 19;
+            string nomeCompleto = "Beatriz Silva Queiroz";
+
+            var variavelSemTipo = "Beatriz";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+
+            Console.WriteLine("digite seu nome: ");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("digite a sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"meu nome é {nomeUsuario} e a minha idade é {idade}");
+        }
+    }
+}
